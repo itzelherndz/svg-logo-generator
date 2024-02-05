d821f276a88506b0c2a9a7d0b9ce08855e04199b
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-02-05 10:47:27 -0700

    renamed shapes files and class to logo

--- test/logo.test.js
+++ test/logo.test.js
@@ -1,83 +1,83 @@
-// Imports Shapes class
-const Shapes = require('../lib/shapes');
+// Imports Logo class
+const Logo = require('../lib/logo');
 
-// Shapes testing suite
-describe('Shapes',() => {
-    // Tests the instance of Shapes class
+// Logo testing suite
+describe('Logo',() => {
+    // Tests the instance of Logo class
     describe('Instantiate', () => {
-        it('should be an instance of Shapes class', () => {
-            const shape = new Shapes();
-            expect(shape).toBeInstanceOf(Shapes);
+        it('should be an instance of Logo class', () => {
+            const logo = new Logo();
+            expect(logo).toBeInstanceOf(Logo);
         });
     });
     // Tests that the shape color is initialized
     describe('Initialize shape color', () => {
-        it('should set color properly', () => {
-            const color = 'green';
-            const shape = new Shapes(color);
-            expect(shape.color).toBe(color);
+        it('should set shapeColor properly', () => {
+            const shapeColor = 'green';
+            const logo = new Logo(shapeColor);
+            expect(logo.shapeColor).toBe(shapeColor);
         });
     });
     // Tests that text is initialized
     describe('Initialize text', () => {
         it('should set text properly', () => {
             const text = 'text';
-            const shape = new Shapes('green',text);
-            expect(shape.text).toBe(text);
+            const logo = new Logo('green',text);
+            expect(logo.text).toBe(text);
         });
     });
     // Tests that text color is initialized
     describe('Initialize text color', () => {
         it('should set textColor properly', () => {
             const textColor = 'white';
-            const shape = new Shapes('green','text',textColor);
-            expect(shape.textColor).toBe(textColor);
+            const logo = new Logo('green','text',textColor);
+            expect(logo.textColor).toBe(textColor);
         });
     });
     // Tests that fontFamily is initialized
     describe('Initialize fontFamily', () => {
         it('should set fontFamily properly', () => {
             const fontFamily = 'sans-serif';
-            const shape = new Shapes('green','text','white',fontFamily);
-            expect(shape.fontFamily).toBe(fontFamily);
+            const logo = new Logo('green','text','white',fontFamily);
+            expect(logo.fontFamily).toBe(fontFamily);
         });
     });
     // Tests the functionality of fontSize method
     describe('fontSize method', () => {
         it('should return font size of 150 for text length of 1', () => {
             const text = 'A';
-            const shape = new Shapes('white',text);
-            expect(shape.fontSize()).toEqual(150);
+            const logo = new Logo('white',text);
+            expect(logo.fontSize()).toEqual(150);
         });
         it('should return font size of 120 for text length of 2', () => {
             const text = 'AB';
-            const shape = new Shapes ('white',text);
-            expect(shape.fontSize()).toEqual(120);
+            const logo = new Logo ('white',text);
+            expect(logo.fontSize()).toEqual(120);
         });
         it('should return font size of 75 for text length of 3', () => {
             const text = 'ABC';
-            const shape = new Shapes ('white',text);
-            expect(shape.fontSize()).toEqual(75);
+            const logo = new Logo ('white',text);
+            expect(logo.fontSize()).toEqual(75);
         });
         it('should return font size of 55 for text length of 4', () => {
             const text = 'ABCD';
-            const shape = new Shapes ('white',text);
-            expect(shape.fontSize()).toEqual(55);
+            const logo = new Logo ('white',text);
+            expect(logo.fontSize()).toEqual(55);
         });
         it('should return font size of 40 for text length of 5', () => {
             const text = 'ABCDE';
-            const shape = new Shapes ('white',text);
-            expect(shape.fontSize()).toEqual(40);
+            const logo = new Logo ('white',text);
+            expect(logo.fontSize()).toEqual(40);
         });
         it('should return font size of 25 for text length of 0', () => {
             const text = '';
-            const shape = new Shapes ('white', text);
-            expect(shape.fontSize()).toEqual(25);
+            const logo = new Logo ('white', text);
+            expect(logo.fontSize()).toEqual(25);
         });
         it('should retrun font size of 25 for text length greater than 5', () => {
             const text = 'ABCDEF';
-            const shape = new Shapes ('white', text);
-            expect(shape.fontSize()).toEqual(25);
+            const logo = new Logo ('white', text);
+            expect(logo.fontSize()).toEqual(25);
         });
     });
 });
